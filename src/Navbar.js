1cde91e5ee259e4a7bb3865d47ef2d26635a3901
Richard Ingwe <richardingwe5@gmail.com>
2021-01-03 23:33:44 +0100

    consuming multiple context with hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,7 +9,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavbarStyles";
 import { ThemeContext } from "./contexts/ThemeContext";
-import { withLanguageContext } from "./contexts/LanguageContext";
+import { LanguageContext } from "./contexts/LanguageContext";
 
 const content = {
     english: {
@@ -25,46 +25,43 @@ const content = {
         flag: "ES"
     }
 };
+function Navbar(props) {
 
-class Navbar extends Component {
-    static contextType = ThemeContext;
+    const { isDarkMood, toggleTheme } = useContext(ThemeContext);
+    const { language } = useContext(LanguageContext);
+    const { classes } = props;
+    const { search, flag } = content[language];
 
-    render() {
-        const { isDarkMood, toggleTheme } = this.context;
-        const { classes } = this.props;
-        const { language } = this.props.languageContext;
-        const { search, flag } = content[language];
-
-        return (
-            <div className={classes.root}>
-                <AppBar postion="static" color={isDarkMood ? "default" : "primary"}>
-                    <ToolBar>
-                        <IconButton className={classes.newButton} color="inherit">
-                            <span>{flag}</span>
-                        </IconButton>
-                        <Typography
-                            className={classes.title}
-                            variant="h6"
-                            color="inherit"
-                        >
-                            App Title
+    return (
+        <div className={classes.root}>
+            <AppBar postion="static" color={isDarkMood ? "default" : "primary"}>
+                <ToolBar>
+                    <IconButton className={classes.newButton} color="inherit">
+                        <span>{flag}</span>
+                    </IconButton>
+                    <Typography
+                        className={classes.title}
+                        variant="h6"
+                        color="inherit"
+                    >
+                        App Title
                         </Typography>
-                        <Switch onClick={toggleTheme} />
-                        <div className={classes.grow} />
-                        <div className={classes.search}>
-                            <div className={classes.seachIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder={`${search}...`}
-                                classes={{
-                                    root: classes.inputRoot,
-                                    input: classes.inputInput
-                                }} />
+                    <Switch onClick={toggleTheme} />
+                    <div className={classes.grow} />
+                    <div className={classes.search}>
+                        <div className={classes.seachIcon}>
+                            <SearchIcon />
                         </div>
-                    </ToolBar>
-                </AppBar>
-            </div>
-        );
-    }
+                        <InputBase placeholder={`${search}...`}
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput
+                            }} />
+                    </div>
+                </ToolBar>
+            </AppBar>
+        </div>
+    );
 }
-export default withLanguageContext(withStyles(styles)(Navbar));
+
+export default withStyles(styles)(Navbar);
